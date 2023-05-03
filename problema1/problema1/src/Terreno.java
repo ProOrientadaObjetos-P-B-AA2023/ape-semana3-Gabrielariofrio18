a320e70de43a660368ee2346e55bdaa0c01950b7
Gabrielariofrio18 <116389985+Gabrielariofrio18@users.noreply.github.com>
2023-05-02 23:34:01 -0500

    Add files via upload

---
+++ problema1/problema1/src/Terreno.java
@@ -0,0 +1,11 @@
+public class ejecutor {
+    }
+    class Problema1 {
+        public static void main(String[] args) {
+            Terreno terreno1 = new Terreno();
+            terreno1.calcularAreaTerreno();
+            terreno1.calcularCostoTerreno();
+            System.out.println(terreno1);
+        }
+    }
+
